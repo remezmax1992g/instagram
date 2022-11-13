b8497b4df919aa4380ca0423efaec29d999647bd
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-14 00:50:25 +0300

    generating of new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {Test1Component} from "./test1/test1.component";
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './user/components/test4/test4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test1Component
+    Test1Component,
+    Test2Component,
+    Test3Component,
+    Test4Component
   ],
   imports: [
     BrowserModule
