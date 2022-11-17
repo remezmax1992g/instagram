d9916812a3326ac161d54e86e1ec39e4c9b0dc0b
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-17 19:59:29 +0300

    visible scope of services

---
+++ src/app/components/comp-a/comp-a.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core'
+import { ValueService } from '../../services/value.service'
+
+@Component({
+  selector: 'inst-comp-a',
+  templateUrl: './comp-a.component.html',
+  styleUrls: ['./comp-a.component.css'],
+})
+export class CompAComponent implements OnInit {
+  value = 0
+  constructor(private valueService: ValueService) {}
+
+  ngOnInit(): void {
+    this.value = this.valueService.value
+  }
+  incValueHandler() {
+    this.valueService.inc()
+  }
+}
---
+++ src/app/components/comp-b/comp-b.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core'
+import { ValueService } from '../../services/value.service'
+
+@Component({
+  selector: 'inst-comp-b',
+  templateUrl: './comp-b.component.html',
+  styleUrls: ['./comp-b.component.css'],
+})
+export class CompBComponent implements OnInit {
+  value = 0
+
+  constructor(private valueService: ValueService) {}
+
+  ngOnInit(): void {
+    this.value = this.valueService.value
+  }
+
+  decValueHandler() {
+    this.valueService.dec()
+  }
+}
