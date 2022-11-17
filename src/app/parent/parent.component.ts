bddb960036de93d9e7c7c8aab0718eda87f27426
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-17 16:08:45 +0300

    directive - *ngFor - big object

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core'
 
+interface Lesson {
+  id: number
+  title: string
+  weekGrade: WeekGrade[]
+}
+
+interface WeekGrade {
+  id: number
+  gradeItem: number
+}
+
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
@@ -7,6 +18,62 @@ import { Component } from '@angular/core'
 })
 export class ParentComponent {
   grades: string[] = ['math: 5', 'english: 4']
+  lessons: Lesson[] = [
+    {
+      id: 0,
+      title: 'math',
+      weekGrade: [
+        {
+          id: 0,
+          gradeItem: 5,
+        },
+        {
+          id: 1,
+          gradeItem: 4,
+        },
+        {
+          id: 2,
+          gradeItem: 5,
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: 'physic',
+      weekGrade: [
+        {
+          id: 0,
+          gradeItem: 5,
+        },
+        {
+          id: 1,
+          gradeItem: 3,
+        },
+        {
+          id: 2,
+          gradeItem: 5,
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'english',
+      weekGrade: [
+        {
+          id: 0,
+          gradeItem: 5,
+        },
+        {
+          id: 1,
+          gradeItem: 3,
+        },
+        {
+          id: 2,
+          gradeItem: 5,
+        },
+      ],
+    },
+  ]
 
   getGrade(value: string) {
     this.grades.push(value)
