da6e7d1cb6fb86e69f2c6abb38a2743648495e23
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-17 19:44:04 +0300

    creating of services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
-import { ParentComponent } from './parent/parent.component'
-import { ChildComponent } from './parent/child/child.component'
+import { CompAComponent } from './components/comp-a/comp-a.component'
+import { CompBComponent } from './components/comp-b/comp-b.component'
 
 @NgModule({
-  declarations: [AppComponent, ParentComponent, ChildComponent],
+  declarations: [AppComponent, CompAComponent, CompBComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/parent/child/child.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'inst-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css'],
-})
-export class ChildComponent {}
